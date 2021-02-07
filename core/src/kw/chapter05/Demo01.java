922b7e82beebb409fdcf4b972a9c81a7552a16d8
kw <2818815189@qq.com>
2021-02-07 19:38:14 +0800

    提交笔记

--- core/src/kw/chapter05/Demo01.java
+++ core/src/kw/chapter05/Demo01.java
@@ -1,5 +1,7 @@
 package kw.chapter05;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -12,10 +14,10 @@ import kw.utils.Utils;
 public class Demo01 extends Group {
     public Demo01(){
         Utils utils = new Utils();
-        utils.createBox(10,10,Constant.width,1,true);
-
-        utils.createBox(10,40,10,10,false);
-
+        Body box = utils.createBox(Constant.width/2 + 10, 10, Constant.width / 2, 1, true);
+        box.setUserData("box1");
+        Body box1 = utils.createBox(20, 40, 10, 10, false);
+        box1.setUserData("box2");
         Constant.world.setContactListener(new ContactListener() {
 
             /**
@@ -26,19 +28,32 @@ public class Demo01 extends Group {
             @Override
             public void beginContact(Contact contact) {
                 System.out.println("begin");
+//                System.out.println("contact.getFixtureA()"+contact.getFixtureA().getBody().getUserData());
+//                System.out.println("contact.getFixtureB()"+contact.getFixtureB().getBody().getUserData());
+                //
+                Vector2[] points = contact.getWorldManifold().getPoints();
+                System.out.println(points[0].x+"======"+points[0].y);
+
+                //是不是发生重叠   挨住
+                System.out.println(contact.isTouching());
+
+                String userData = (String) contact.getFixtureB().getBody().getUserData();
+
+
             }
 
             @Override
             public void endContact(Contact contact) {
-                System.out.println("end");
-                System.out.println();
+//                System.out.println("end");
+//                System.out.println();
             }
 
             //begin开始之后就执行   一直到不活跃
             @Override
             public void preSolve(Contact contact, Manifold oldManifold) {
-                System.out.println("pre");
-                System.out.println();
+                contact.setEnabled(false);
+//                System.out.println("pre");
+//                System.out.println();
             }
 
 
@@ -49,7 +64,7 @@ public class Demo01 extends Group {
              */
             @Override
             public void postSolve(Contact contact, ContactImpulse impulse) {
-                System.out.println("post");
+//                System.out.println("post");
             }
         });
     }
